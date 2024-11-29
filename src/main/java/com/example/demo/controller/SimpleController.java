b74e7cbb2f813fae7b8d5a51b94e392858e96f4e
Tolya <tolya@sablin.xyz>
2024-11-29 14:04:50 +0300

    Update dependencies.

--- src/main/java/com/example/demo/controller/SimpleController.java
+++ src/main/java/com/example/demo/controller/SimpleController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/**")
 public class SimpleController {
-
-    private final JavaMailSender mailSender;
-
-    public SimpleController(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
-    }
 
     @PreAuthorize("hasAnyRole('GROUP1')")
     @GetMapping
